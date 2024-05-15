dd58a8228031ff2f55bf7e43471e4fc95e472683
dariokempf <dario.kempf@edu.vs.ch>
2024-05-15 15:25:53 +0200

    overwrited online

--- Backend-ToDo/app/Database/Migrations/2024-05-01-142520_Todo.php
+++ Backend-ToDo/app/Database/Migrations/2024-05-01-142520_Todo.php
@@ -8,19 +8,18 @@ class Todo extends Migration
 {
     public function up()
     {
-        $this->db->query("CREATE TABLE todo
+        $this->db->query("CREATE TABLE `todo`
         (
-            id INT(11) UNSIGNED AUTO_INCREMENT,
-            user_id INT(11),
-            titel VARCHAR(255),
-            content VARCHAR(255),
-            tag TEXT,
-            done BOOLEAN,
-            created_at DATETIME,
-            updated_at DATETIME,
-            deleted_at DATETIME,
-            PRIMARY KEY (id),
-            FOREIGN KEY (user_id)
+            `id` INT(11) UNSIGNED AUTO_INCREMENT,
+            `user_id` INT(11),
+            `titel` VARCHAR(255),
+            `content` VARCHAR(255),
+            `tag` TEXT,
+            `done` BOOLEAN,
+            `created_at` DATETIME,
+            `updated_at` DATETIME,
+            `deleted_at` DATETIME,
+            PRIMARY KEY (`id`)
             );
         ");
     }
