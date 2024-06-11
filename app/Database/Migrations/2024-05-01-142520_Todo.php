4091d0adab90233e71163c459b70aea4e9c16efd
Ivo Heberle <ivo.heberle@edu.vs.ch>
2024-06-11 10:57:10 +0200

    contiue work

--- app/Database/Migrations/2024-05-01-142520_Todo.php
+++ app/Database/Migrations/2024-05-01-142520_Todo.php
@@ -22,10 +22,37 @@ class Todo extends Migration
             PRIMARY KEY (`id`)
             );
         ");
+        // $this->forge->addField([
+        //     "id"=> [
+        //         "type"=> "INT",
+        //         "auto_increment"=> true
+        //     ],
+        //     "user_id"=> [
+        //         "type"=> "INT",
+        //     ],
+        //     "title"=> [
+        //         "type"=> "VARCHAR",
+        //         "constraint"=> "255"
+        //     ],
+        //     "content"=> [
+        //         "type"=> "VARCHAR",
+        //         "constraint"=> "255"
+        //     ],
+        //     "tag"=> [
+        //         "type"=> "TEXT",
+        //     ],
+        //     "done"=> [
+        //         "type"=> "BOOLEAN",
+        //     ],
+        //     'created_at datetime default current_timestamp',
+        //     'updated_at datetime default current_timestamp on update current_timestamp',
+        // ]);
+        // $this->forge->addKey("id", true);
+        // $this->forge->createTable("todos");
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable("todo");
     }
 }
